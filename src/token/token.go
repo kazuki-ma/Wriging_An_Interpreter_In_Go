8528a6dc322eeba3482a5f0e9480affacd5b115e
Kazuki MATSUDA / 松田一樹 <matsuda.kazuki@gmail.com>
2018-07-26 01:05:07 +0900

    1.3 - The Lexer

--- src/token/token.go
+++ src/token/token.go
@@ -1,4 +1,4 @@
-package main
+package token
 
 type TokenType string
 type Token struct {
@@ -28,3 +28,15 @@ const (
 
     LET     = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
