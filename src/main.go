878d85ec52bd207995ec4f62eeb1de8845a79ef7
Kazuki MATSUDA / 松田一樹 <matsuda.kazuki@gmail.com>
2018-07-29 00:26:28 +0900

    2.4 - Parser's first steps: parsing let statements

--- src/main.go
+++ src/main.go
@@ -5,17 +5,16 @@ import "os"
 import "os/user"
 import "./repl"
 
-
 func main() {
-    user, err := user.Current()
-    if (err != nil) {
-        panic(err)
-    }
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-                user.Username);
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
 
-    fmt.Printf("Feel free to type commands\n")
+	fmt.Printf("Feel free to type commands\n")
 
-    repl.Start(os.Stdin, os.Stdout)
+	repl.Start(os.Stdin, os.Stdout)
 }
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -1,31 +1,31 @@
 package repl
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "../lexer"
-    "../token"
+	"../lexer"
+	"../token"
+	"bufio"
+	"fmt"
+	"io"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-    scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
-    for {
-        fmt.Printf(PROMPT)
-        scanned := scanner.Scan()
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
 
-        if !scanned {
-            return
-        }
+		if !scanned {
+			return
+		}
 
-        line := scanner.Text()
-        l := lexer.New(line)
+		line := scanner.Text()
+		l := lexer.New(line)
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Printf("%+v\n", tok);
-        }
-    }
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
 }
