ec7cd241f6da0cf0d431034344be0735e54872e4
Kazuki MATSUDA / 松田一樹 <matsuda.kazuki@gmail.com>
2018-07-29 13:08:18 +0900

    Before Prefix Operators

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -80,6 +80,19 @@ func (i *Identifier) String() string {
 	return i.Value
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (i *IntegerLiteral) expressionNode() {}
+func (i *IntegerLiteral) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *IntegerLiteral) String() string {
+	return i.Token.Literal
+}
+
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
